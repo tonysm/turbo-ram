b081dea96166183f45f9a4fe532a0b2c16074510
Tony Messias <tonysm@hey.com>
2022-03-07 22:10:32 -0300

    Fix the wrong BucketPostsController

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ Route::middleware(['auth:sanctum', 'verified'])->group(function () {
 
     Route::resource('buckets.blogs', Controllers\BucketBlogsController::class)->only(['show']);
     Route::resource('buckets.blogs.posts', Controllers\BucketBlogPostsController::class)->only(['index', 'create', 'store']);
-    Route::resource('buckets.posts', Controllers\BucketPostscontroller::class)->only(['show', 'edit', 'update', 'destroy']);
+    Route::resource('buckets.posts', Controllers\BucketPostsController::class)->only(['show', 'edit', 'update', 'destroy']);
     Route::resource('buckets.recordings.comments', Controllers\BucketRecordingCommentsController::class)->only(['index', 'create', 'store']);
     Route::resource('buckets.comments', Controllers\BucketCommentsController::class)->only(['show', 'edit', 'update', 'destroy']);
 });
