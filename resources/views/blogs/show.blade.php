1f709dfb95a065d0dc021ca8c02653831337c6c9
Tony Messias <tonysm@hey.com>
2022-03-15 00:47:02 -0300

    Hide the new post btn on mobile screens

--- resources/views/blogs/show.blade.php
+++ resources/views/blogs/show.blade.php
@@ -1,19 +1,19 @@
 <x-app-layout>
     <x-slot name="header">
         <div class="flex items-center justify-between">
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            <h2 class="text-xl font-semibold leading-tight text-gray-800">
                 {{ __('Posts') }}
             </h2>
 
-            <div>
+            <div class="hidden sm:block">
                 <a href="{{ route('buckets.blogs.posts.create', [$bucket, $blog]) }}">New Post</a>
             </div>
         </div>
     </x-slot>
 
     <div class="sm:py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+        <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+            <div class="overflow-hidden bg-white shadow-xl sm:rounded-lg">
                 <div class="p-8 sm:px-20">
                     <div class="flex flex-col divide-y">
                         @each('bucket_posts._post_card', $posts, 'recording')
--- resources/views/recordings/_comment_form_trigger.blade.php
+++ resources/views/recordings/_comment_form_trigger.blade.php
@@ -1,17 +1,17 @@
-<div class="flex border-t border-gray-100 py-8" id="@domid($recording, 'create_comment_trigger')">
-    <div class="hidden sm:block w-1/6">&nbsp;</div>
+<div class="hidden py-8 border-t border-gray-100 sm:flex" id="@domid($recording, 'create_comment_trigger')">
+    <div class="hidden w-1/6 sm:block">&nbsp;</div>
 
-    <div class="sm:w-5/6 flex items-start gap-2 text-lg">
+    <div class="flex items-start gap-2 text-lg sm:w-5/6">
         <x-user-avatar :user="Auth::user()" />
 
         <div class="w-full">
-            <x-turbo-frame :id="[$recording, 'create_comment']" class="hidden sm:block w-full">
-                <a href="{{ route('buckets.recordings.comments.create', [$recording->bucket, $recording]) }}" class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring focus:ring-gray-300 disabled:opacity-25 transition">
+            <x-turbo-frame :id="[$recording, 'create_comment']" class="hidden w-full sm:block">
+                <a href="{{ route('buckets.recordings.comments.create', [$recording->bucket, $recording]) }}" class="inline-flex items-center px-4 py-2 text-xs font-semibold tracking-widest text-white uppercase transition bg-gray-800 border border-transparent rounded-md hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring focus:ring-gray-300 disabled:opacity-25">
                     <span>New Comment</span>
                 </a>
             </x-turbo-frame>
 
-            <a href="{{ route('buckets.recordings.comments.create', [$recording->bucket, $recording]) }}" class="inline-flex sm:hidden items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring focus:ring-gray-300 disabled:opacity-25 transition">
+            <a href="{{ route('buckets.recordings.comments.create', [$recording->bucket, $recording]) }}" class="inline-flex items-center px-4 py-2 text-xs font-semibold tracking-widest text-white uppercase transition bg-gray-800 border border-transparent rounded-md sm:hidden hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring focus:ring-gray-300 disabled:opacity-25">
                 <span>New Comment</span>
             </a>
         </div>
